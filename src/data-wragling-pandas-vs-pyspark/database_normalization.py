5481447cde358aba34a2dc941ce61aa0300beffe
Mete Can Akar <metecanakar@gmail.com>
2024-03-17 10:34:16 +0100

    database normalization implemented

--- src/data-wragling-pandas-vs-pyspark/database_normalization.py
+++ src/data-wragling-pandas-vs-pyspark/database_normalization.py
@@ -1,3 +1,49 @@
+"""
+Data normalization example (1NF, 2NF, 3NF) using pyspark.
+
+Vocabulary:
+1) Candidate Keys: These are all the keys in a table that can uniquely identify each row. There can be multiple candidate keys in a table.
+ Each candidate key can be a single column or a combination of columns (making it a composite candidate key if it involves more than one column).
+ Example:
+    -UserID: Can uniquely identify each user in the system.
+    -SocialSecurityNumber: Also uniquely identifies each individual, hence each user.
+    -EmailAddress: Unique to each user and can be used to identify them.
+    -Username: Intended to be unique for each user.
+2) Composite Key: This is a type of key that consists of two or more columns because no single column can uniquely identify each row by itself.
+ A composite key can be a candidate key if it uniquely identifies each row, and it can also be selected as the primary key.
+ Example:
+    -Suppose, in a hypothetical scenario, none of the above were individually unique due to system legacy issues,
+      but the combination of EmailAddress and Username together is unique for all users.
+     This combination would be a composite key because it requires both fields together to uniquely identify a record.
+3) Primary Key: From the set of candidate keys, one is chosen to be the primary key. 
+The primary key is the key that the database system uses to uniquely identify each row in a table.
+ A primary key can be a single column or a composite of multiple columns.
+ Example:
+ -Let's choose UserID as the primary key. It's common to use a simple, system-generated identifier like UserID
+   as the primary key because it is guaranteed to be unique, it's simple,
+   and its value doesn't carry meaningful information that could change over time (unlike, say, an email address or SSN).
+
+To summarize, a primary key is selected from among the candidate keys, and it uniquely identifies each row in a table.
+ If a primary key consists of more than one column, it is both a composite key and a composite candidate key chosen to serve as the primary key.
+
+4) Functional dependency (Partial or Fully)
+General name for functional dependency. It can be partial or full functional dependency. See below:
+
+5) Partial Functional Dependency
+A partial functional dependency occurs in the context of composite keys (where the primary key consists of two or more attributes) and indicates that an attribute outside the composite key is functionally dependent on part of the composite key, rather than the whole key.
+
+Example: In an OrderDetails table with a composite primary key consisting of OrderID and ProductID:
+if there's a column OrderDate, and OrderDate is determined by OrderID alone, then OrderID -> OrderDate is a partial functional dependency because OrderDate depends only on part of the composite key (OrderID, not both OrderID and ProductID).
+
+6) Full Functional Dependency
+A fully functional dependency means that an attribute is fully functionally dependent on a whole composite key rather than just a part of it.
+Or an attribute is fully functionally dependent on 1 primary key.
+Example:
+OrderID, ProductID => OrderQuantity
+Example2:
+StudentID => StudentName
+"""
+
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 
@@ -59,14 +105,18 @@ df_1nf.show()
 
 
 # ----------------- 2NF start -----------------
+# Functional dependency: The value of one of the attribute (the determinant) determines the value of another attribute.
+# Each non-key field is functionally dependent on every candidate key.
+
 # 2NF requires removing partial functional dependencies, where an attribute is functionally dependent on part of a composite primary key. Transitive dependencies are allowed.
 # Therefore, we need to convert all the partial functional dependencies to fully functional dependencies.
 # In the output of 1NF , the composite primary key is (OrderID, ProductID).
-# OrderDate, CustomerID, CustomerName, and CustomerAddress are only functionally dependent on OrderID, not on ProductID. Therefore, they are partially functionally dependent on the composite primary key.
+# OrderDate, CustomerID, CustomerName, and CustomerAddress are only functionally dependent on OrderID, not on ProductID. Therefore, they are partially functionally dependent on the composite primary key. In other words, OrderID uniquely identifies these 4 columns.
 # Price only functionally depends on ProductID, not on OrderID. Therefore, it is partially functionally dependent on the composite primary key.
 # However, to define the OrderQuantity, we have to know both the OrderID and the ProductID. Therefore, the OrderQuantity is fully functionally dependent on the composite primary key (OrderID, ProductID).
 # Now, we need to create separate tables for Order and Product, to remove partial functional dependency and create fully functional dependency in each table.
 
+# Side Note: A candidate key determines the whole table. A candidate key can be a composite key as well (e.g., AB => CDE)
 # Orderline table (3NF)
 orderline = df_1nf.select("OrderID", "ProductID", "OrderQuantity")
 orderline.show()
